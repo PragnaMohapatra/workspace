87d9fb3a0e0a08335d8241ea7a537a66fa7ebb1f
PragnaMohapatra <PragnaMohapatra@gmail.com>
2016-02-26 21:46:48 -0500

    Added files via upload

---
+++ Stack.java
@@ -0,0 +1,63 @@
+
+public class Stack {
+	int size=0;
+	int capacity;
+	int[] storage;
+
+	public Stack(int capacity) {
+		this.capacity = capacity;
+		storage = new int[capacity];
+	}
+
+	public void put(int num) {
+		System.out.println("Size :: "+(size));
+		System.out.println("Capacity :: "+ capacity);
+		if(size==capacity){
+			System.out.println("The Stack is full");
+			return;
+		}
+		storage[size]=num;
+		size++;
+	}
+	
+	public int pop(){
+		int value=0;
+		if(size==0){
+			System.out.println("The statck is empty");
+			return 0;
+		}
+		size--;
+		value = storage[size];
+		return value;
+	}
+	
+	public int peek(){
+		if(size==0){
+			System.out.println("The statck is siz");
+		}
+		return storage[size];
+	}
+	
+	public static void main(String... args){
+		Stack stack = new Stack(10);
+		stack.put(0);
+		stack.put(1);
+		stack.put(2);
+		stack.put(3);
+		stack.put(4);
+		stack.put(5);
+		stack.put(6);
+		stack.put(7);
+		stack.put(8);
+		stack.put(9);
+		stack.put(10);
+		
+		System.out.println(stack);
+		
+		for(int i=0;i<11;i++){
+			
+			System.out.println(stack.pop());
+		}
+		
+	}
+}
