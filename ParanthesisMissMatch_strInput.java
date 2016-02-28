d330cf553ee387ba5a6ceaa6d57ed46ed9ab148e
PragnaMohapatra <PragnaMohapatra@gmail.com>
2016-02-27 19:08:01 -0500

    Added files via upload

--- ParanthesisMissMatch_strInput.java
+++ ParanthesisMissMatch_strInput.java
@@ -7,7 +7,7 @@ public class ParanthesisMissMatch_strInput {
 
 	public static void main(String[] args) {
 		ParanthesisMissMatch_strInput input = new ParanthesisMissMatch_strInput();
-		System.out.println(input.isValid("((((((()))))))"));
+		System.out.println(input.isValid("()[]{}"));
 
 	}
 
@@ -20,23 +20,38 @@ public class ParanthesisMissMatch_strInput {
 		if (s.length() % 2 != 0) {
 			return false;
 		}
-		boolean valid = true;
 		Map<Character, Character> dict = new HashMap<Character, Character>();
-		java.util.Stack<Character> stack = new java.util.Stack<Character>();
 		Queue<Character> queue = new LinkedList<Character>();
 		dict.put('{', '}');
 		dict.put('[', ']');
 		dict.put('(', ')');
+		
+		if(dict.get(s.charAt(0))==null){
+			return false;
+		}
+		boolean valid = true;
+		
+		java.util.Stack<Character> stack = new java.util.Stack<Character>();
+		
 		boolean flip = false;
 		for (int i = 0; i < s.length(); i++) {
 			if (dict.get(s.charAt(i)) != null && flip) {
-				while (!stack.isEmpty()) {
-					if (dict.get(stack.pop()) != queue.poll()) {
+				while (true) {
+					if (stack.isEmpty()&& dict.get(stack.peek()) != queue.peek()) {
 						valid = false;
 						return valid;
+					} else if (dict.get(stack.peek()) == queue.peek()){
+						stack.pop();
+						queue.poll();
+						if(stack.isEmpty()){
+							stack.add(s.charAt(i));
+							break;
+						}
+					} else {
+						stack.add(s.charAt(i));
+						break;
 					}
 				}
-				stack.add(s.charAt(i));
 				flip = false;
 			}
 			else if (dict.get(s.charAt(i)) != null) {
