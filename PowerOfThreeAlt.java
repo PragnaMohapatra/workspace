6c7036c92d2aaa89a50c88b3494e8bcdc8e33150
PragnaMohapatra <PragnaMohapatra@gmail.com>
2016-03-08 23:32:37 -0500

    Added files via upload

---
+++ PowerOfThreeAlt.java
@@ -0,0 +1,25 @@
+package com.amazon.interview1;
+
+public class PowerOfThreeAlt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(isPowerOfThree(19682));
+	}
+
+	public static boolean isPowerOfThree(int n) {
+
+		if (n == 3 || n == 1) {
+			return true;
+		}
+		if(n%3!=0){
+			return false;
+		}
+		double val = Math.log(n) / Math.log(3);
+		double rounded = (double) Math.round(val * 10000) / 10000;
+		if (Math.ceil(val) - rounded == 0) {
+			return true;
+		}
+		return false;
+	}
+}
